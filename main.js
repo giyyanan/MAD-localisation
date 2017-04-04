8dae54adead2f18f29c705d396f56d07113c5997
GIV <giyyanan@andrew.cmu.edu>
2017-04-04 09:55:32 -0700

    un-deployed device info updated

--- main.js
+++ main.js
@@ -5,28 +5,73 @@ var config = {
 	storageBucket: "ad-hoc-building-sensor-devices.appspot.com",
 };
 
-var instance_id = "-KYGUYsamLdgQ6TpDX3v";
+var instance_id;//="-KYGUYsamLdgQ6TpDX3v";
 var device_store;
-var devices = [];
+var devices = {};
+var devices_count = {};
+var material_icons = {
+	"FLASH":'<i class="material-icons">highlight</i>',
+	"LIGHT":'<i class="material-icons">lightbulb_outline</i>',
+	"TEMPERATURE":'<i class="ionicons ion-thermometer"></i>',
+
+}
 var roomDiv;
 var buildRoom = function(){
+	connectFirebase();
 	roomDiv = document.getElementById('room');
-	fetchDevicesforInstance();
+	if(!instance_id){
+		fetchInstanceId();
+	}
+	if(instance_id){
+		fetchDevicesforInstance();
+	}
 	//console.log(devices)
 	
 }
+var fetchInstanceId = function(){
+	$('#myModal').modal('show');
+}
+var updateInstanceId = function(){
+	var instId = document.getElementById("instanceID").value;
+
+	if(instId){
+		firebase.database().ref('devices/').once('value', function(snapshot) {
+			device_store=snapshot.val();
+			/*snapshot.forEach(function(childSnapshot) {
+				var childKey = childSnapshot.key;
+				var childKey = childSnapshot.key;
+				childData = childSnapshot.val();
+				instances.push(String(childKey));
+			});*/
+			if(device_store.hasOwnProperty(instId)){
+				instance_id = instId;
+				$('#myModal').modal('hide');
+				fetchDevicesforInstance();
+			}
+			else{
+				alert("Instance id not found in firebase");
+				document.getElementById("instanceID").value = "";
+			}
+		});
+		
+	}
+	else{
+		alert("instance Id is not entered");
+	}
+
+}
 var fetchDevicesforInstance = function()
-{
-	if(connectFirebase())
+{ 
+	if(instance_id)
 	{
 		firebase.database().ref('devices/'+instance_id).once('value', function(snapshot) {
 			device_store=snapshot.val();
-			snapshot.forEach(function(childSnapshot) {
+			/*snapshot.forEach(function(childSnapshot) {
 				var childKey = childSnapshot.key;
 				childData = childSnapshot.val();
-				devices.push(childKey);
+				devices[childKey] = childData;
 
-			});
+			});*/
 			addDevices();
 		});
 	}
@@ -39,27 +84,47 @@ var addDevices = function(){
 	var deviceDiv = document.getElementById("device");
 	var devicesDiv = document.getElementById("devices");
 	var cloneDiv;
+	var devices = device_store;
+
 	for(device in devices){
 		console.log(devices[device]);
 		cloneDiv = deviceDiv.cloneNode(true);
-		cloneDiv.id=devices[device];
-		if(device_store[devices[device]].hasOwnProperty('location')){
+		cloneDiv.id=String(device);
+		device_type = devices[device].config[Object.keys(devices[device].config)[0]].hasOwnProperty("type") ? devices[device].config[Object.keys(devices[device].config)[0]].type : "NONE"
+		cloneDiv.innerHTML = material_icons[device_type];
+		if(!(device_type in devices_count) )
+			devices_count[device_type] = 0;
+		
+		if(devices[device].hasOwnProperty('location')){
+			
+			cloneDiv.style.left = devices[device].location.coordinates.X;
+			cloneDiv.style.top = devices[device].location.coordinates.Y;
+			cloneDiv.style.position = 'absolute';
 			roomDiv.appendChild(cloneDiv);
-			cloneDiv.style.left = device_store[devices[device]].location.coordinates.X;
-			cloneDiv.style.top = device_store[devices[device]].location.coordinates.Y;
+			cloneDiv.innerHTML = material_icons[device_type];
+
 		}
 		else{
 			devicesDiv.appendChild(cloneDiv);
+			devices_count[device_type] += 1;
 		}
 	}
+	updateDeviceCount();
+
 	
 
 }
+var updateDeviceCount = function(){
+	var devicesCount = document.getElementById("devices_count");
+	devicesCount.innerHTML="";
+	for(types in devices_count){
+		devicesCount.innerHTML += " "+ String(types)+ " : "+ String(devices_count[types]) 
 
+	}
+}
 var connectFirebase = function(){
 
 	firebase.initializeApp(config);
-
 	return true;
 }
 
@@ -76,13 +141,16 @@ var deviceDropped = function(event){
 	console.log(event)
 	event.preventDefault();
 	var deviceID = event.dataTransfer.getData("id");
-	var device = document.getElementById(deviceID)
+	var device = document.getElementById(deviceID);
 	event.target.appendChild(device);
 	device.style.left = event.offsetX;
 	device.style.top = event.offsetY;
-
+	device.style.position = 'absolute';
 	event.stopPropagation();
-	updateDeviceLocation(deviceID)
+	updateDeviceLocation(deviceID);
+	device_type = device_store[deviceID].config[Object.keys(device_store[deviceID].config)[0]].hasOwnProperty("type") ? device_store[deviceID].config[Object.keys(device_store[deviceID].config)[0]].type : "NONE"
+	devices_count[device_type] -= 1;
+	updateDeviceCount();
 }
 var updateDeviceLocation = function(deviceID){
 	var coordinates = getDeviceLocation(deviceID);
